8cf10387e979c7352454aea8d90e8138cd0f6a4e
Romero Antonio Ramos de Mendonça <romeromendonca22@gmail.com>
2023-12-11 20:51:51 -0300

    Started Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace projeto_final_aps.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
